587fde23675055e5f59b011900e4bff6cb1db268
Ye Yang <1126439586@qq.com>
2020-12-30 10:15:23 +0800

    🤗完成化栈为队

--- src/1.栈/04.[ 面试题 03.04 ] 化栈为队.js
+++ src/1.栈/04.[ 面试题 03.04 ] 化栈为队.js
@@ -4,12 +4,64 @@
  */
 
 // * 思路：
+/**
+ * Initialize your data structure here.
+ */
+var MyQueue = function() {
+    this.stack1 = [];
+    this.stack2 = [];
+};
 
+/**
+ * Push element x to the back of queue. 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.stack1.push(x);
+};
 
+/**
+ * Removes the element from in front of queue and returns that element.
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    if(this.stack2.length === 0) {
+        while(this.stack1.length > 0) {
+            this.stack2.push(this.stack1.pop());
+        }   
+    }
+    return this.stack2.pop();
+};
 
-// 测试用例
-let test = ''
+/**
+ * Get the front element.
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    if(this.stack2.length === 0) {
+        while(this.stack1.length > 0) {
+            this.stack2.push(this.stack1.pop());
+        }   
+    }
+    let result = this.stack2.pop();
+    this.stack2.push(result);
+    return result;
+};
 
-console.time('执行用时');
-console.log(xxx(test));
-console.timeEnd('执行用时');
+/**
+ * Returns whether the queue is empty.
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.stack1.length + this.stack2.length === 0;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
